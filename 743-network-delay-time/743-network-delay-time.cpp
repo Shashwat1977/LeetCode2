2cdc8d0d678ab6f9c6514342756f64ba7910646f
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-04-02 16:36:40 +0530

    Time: 1196 ms (5.02%), Space: 275.3 MB (5.02%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-        vector<int> cost(n+1,INT_MAX);
-        vector<pair<int,int>> adj[n+1];
-        for(auto it:times){
-            adj[it[0]].push_back({it[1],it[2]});
-        }
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        cost[k] = 0;
-        pq.push({0,k});
-        while(!pq.empty()){
-            int curr = pq.top().second;
-            int cos = pq.top().first;
-            pq.pop();
-            for(auto it:adj[curr]){
-                int node = it.first;
-                int wt = it.second;
-                if(cost[node]>cost[curr]+wt){
-                    cost[node] = cost[curr]+wt;
-                    pq.push({cost[node],node});
+        vector<int> dist(n+1,INT_MAX);
+        dist[k] = 0;
+        for(int i = 0;i<n;i++){
+            for(auto edge:times){
+                int currNode = edge[0];
+                int nextNode = edge[1];
+                int wt = edge[2];
+                if(dist[currNode] != INT_MAX && dist[nextNode]>dist[currNode]+wt){
+                    dist[nextNode] = dist[currNode]+wt;
                 }
             }
         }
         int mx = 0;
-        for(int i = 1;i<cost.size();i++){
-            mx = max(mx,cost[i]);
+        for(int i = 1;i<dist.size();i++){
+            mx = max(mx,dist[i]);
         }
         if(mx == INT_MAX) return -1;
         return mx;
