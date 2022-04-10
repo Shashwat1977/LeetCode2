178878f69ae5d14d4d0e01161722d81735c7332d
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-04-10 14:27:56 +0530

    Time: 65 ms (48.80%), Space: 18.9 MB (28.92%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.cpp
+++ 875-koko-eating-bananas/875-koko-eating-bananas.cpp
@@ -5,22 +5,22 @@ public:
         int h = *max_element(piles.begin(),piles.end());
         while(l<=h){
             int mid = l+(h-l)/2;
-            if(feasible(mid,piles,k)){
-                h=mid-1;
+            if(possible(mid,piles,k)){
+                h = mid-1;
             }else{
-                l=mid+1;
+                l = mid+1;
             }
         }
         return l;
     }
-    bool feasible(int mid,vector<int>& piles,int k){
-        int time=0;
+    bool possible(int mid,vector<int>& piles,int k){
+        int time = 0;
         for(auto p : piles){
             time += p/mid;
             if(p%mid) time++;
         }
-        
         if(time<=k) return true;
         return false;
     }
+    
 };
