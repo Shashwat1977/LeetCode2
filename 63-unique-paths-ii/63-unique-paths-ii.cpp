2b03a1cb07877c0ed82b84f75be6d10689863f4b
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-04-20 22:09:28 +0530

    Time: 0 ms (100.00%), Space: 7.8 MB (39.98%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -6,18 +6,18 @@ public:
         vector<vector<int>> dp(m,vector<int>(n,0));
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(g[i][j] == 1) dp[i][j] = 0;
-                else if(i == 0 && j == 0)dp[i][j] = 1;
-                else{
-                    int up =0 ;
+                if(g[i][j] == 1){
+                    dp[i][j] = 0;
+                    continue;
+                }
+                if(i == 0 && j== 0){ dp[i][j] = 1; continue;}
+                int up =0 ;
                     int left = 0;
                     if(i>0) up = dp[i-1][j];
                     if(j>0) left = dp[i][j-1];
                     dp[i][j] = up+left;
-                                         }
-                
+            }
         }
-    }
         return dp[m-1][n-1];
     }
 };
