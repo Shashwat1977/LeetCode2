d0b4609ab5f183af9952905e553fb319c17f58ae
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-06-17 10:46:43 +0530

    Time: 27 ms (33.28%), Space: 15.9 MB (24.36%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -4,11 +4,17 @@ public:
         int l = 0;
         int h = letters.size()-1;
         while(l<=h){
-            int mid = l+(h-l)/2;
-            if(letters[mid]<= target) l = mid+1;
-            else h = mid-1;
+            int m = l+(h-l)/2;
+            if(letters[m] > target){
+                h = m-1;
+            }else{
+                l = m+1;
+            }
         }
-        if(l == letters.size()) return letters[0];
-        return letters[l];
+        if(l == letters.size()){
+            return letters[0];
+        }else{
+            return letters[l];
+        }
     }
 };
