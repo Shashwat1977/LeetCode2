ea818a71a90afa0b0691ea3b5cf40b5af4a74e2f
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-06-20 11:38:57 +0530

    Time: 4 ms (63.70%), Space: 7.9 MB (12.47%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -3,21 +3,20 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
         int n = g.size();
         int m = g[0].size();
-        vector<vector<int>> dp(n,vector<int> (m,0));
-        dp[0][0] = 1;
+        vector<vector<int>> dp(n,vector<int>(m,0));
         for(int i = 0;i<n;i++){
             for(int j = 0;j<m;j++){
-                if(g[i][j] == 1){
-                    dp[i][j] = 0;
-                    continue;
+                if(g[i][j] == 1) dp[i][j] == 0;
+                else{
+                if(i == 0 && j == 0){ dp[i][j] = 1;}
+                else{
+                int l = 0;
+                int u = 0;
+                if(i>0) l = dp[i-1][j];
+                if(j>0) u = dp[i][j-1];
+                dp[i][j] = l+u;
                 }
-                if(i == 0 && j== 0){ dp[i][j] = 1; continue;}
-                int up = 0;
-                int left = 0;
-                if(i>0){
-                    left = dp[i-1][j];
-                }if(j>0) up = dp[i][j-1];
-                dp[i][j] = up+left;
+                }
             }
         }
         return dp[n-1][m-1];
