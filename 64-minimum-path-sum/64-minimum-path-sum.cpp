cf0525e7f154d16364d447f63dbb0ec715c27660
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-06-20 11:54:36 +0530

    Time: 16 ms (40.45%), Space: 10.2 MB (28.15%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.cpp
+++ 64-minimum-path-sum/64-minimum-path-sum.cpp
@@ -6,16 +6,14 @@ public:
         vector<vector<int>> dp(m,vector<int>(n,0));
         for(int i = 0;i<m;i++){
             for(int j = 0;j<n;j++){
-                if(i==0 && j==0) dp[i][j] = grid[i][j];
+                if(i == 0 && j==0) dp[i][j] = grid[i][j];
                 else{
-                    int up = INT_MAX;
-                    int left = INT_MAX;
-                    if(i>0) left = dp[i-1][j];
-                    if(j>0) up = dp[i][j-1];
-                    dp[i][j] = min(left,up)+grid[i][j];
-                                    
+                int up = INT_MAX;
+                int left = INT_MAX;
+                if(i>0) up = dp[i-1][j];
+                if(j>0) left = dp[i][j-1];
+                dp[i][j] = min(up,left)+grid[i][j];
                 }
-                
             }
         }
         return dp[m-1][n-1];
