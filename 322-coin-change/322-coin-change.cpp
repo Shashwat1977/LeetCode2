f7d050abdb53bb0749c8a627f02b2876feaea362
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-06-21 19:59:25 +0530

    Time: 183 ms (24.15%), Space: 45.1 MB (22.64%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -2,25 +2,30 @@ class Solution {
 public:
     int coinChange(vector<int>& arr, int x) {
         int n = arr.size();
-        vector<vector<int>> dp(n,vector<int>(x+1,-1));
-	int ans = helper(n-1,x,arr,dp);
-	if(ans >= 1e9){
-		return -1;
-	}else{
-		return ans;
-	}
+        vector<vector<int>> dp(n,vector<int>(x+1,1e9));
+        for(int i = 0;i<=x;i++){
+            if(i%arr[0] == 0){
+                dp[0][i] = i/arr[0];
+            }
+        }
+        for(int i = 0;i<n;i++){
+            dp[i][0] = 0;
+        }
+        for(int i = 1;i<n;i++){
+            for(int j = 0;j<=x;j++){
+                int no = dp[i-1][j];
+                int pick = 1e9;
+                if(arr[i]<=j){
+                    pick = 1+dp[i][j-arr[i]];
+                }
+                dp[i][j] = min(pick,no);
+            }
+        }
+        if(dp[n-1][x] != 1e9){
+            return dp[n-1][x];
+        }else{
+            return -1;
+        }
     }
-    int helper(int i,int target,vector<int>& arr,vector<vector<int>>& dp){
-	if(i == 0){
-		if(target%arr[0] == 0) return target/arr[0];
-		else return 1e9;
-	}
-	if(dp[i][target] != -1) return dp[i][target];
-	int notpick = helper(i-1,target,arr,dp);
-	int pick = INT_MAX;
-	if(arr[i] <= target){
-		pick = 1+helper(i,target-arr[i],arr,dp);
-	}
-	return dp[i][target] = min(notpick,pick);
-}
+    
 };
