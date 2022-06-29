45d1f81cf571b93d44c6b5a3a0935eb3c102e174
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-06-29 10:57:09 +0530

    Time: 50 ms (16.52%), Space: 14 MB (61.28%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -3,30 +3,30 @@ public:
     vector<int> findOrder(int n, vector<vector<int>>& p) {
         vector<int> adj[n];
         vector<int> indegree(n);
+        vector<int> temp;
         for(auto it:p){
             adj[it[1]].push_back(it[0]);
-        }
-        for(int i = 0;i<n;i++){
-            for(auto it:adj[i]){
-                indegree[it]++;
-            }
+            indegree[it[0]]++;
         }
         queue<int> q;
-        for(int i = 0;i<indegree.size();i++){
+        for(int i = 0;i<n;i++){
             if(indegree[i] == 0) q.push(i);
         }
-        vector<int> temp;
         while(!q.empty()){
             int node = q.front();
-            q.pop();
             temp.push_back(node);
+            q.pop();
             for(auto it:adj[node]){
                 indegree[it]--;
-                if(indegree[it] == 0)q.push(it);
+                if(indegree[it] == 0){
+                    q.push(it);
+                }
             }
         }
-vector<int> tem;
-        if(temp.size() == n) return temp;
-        return tem;
+        if(temp.size() != n){
+            return {};
+        }else{
+            return temp;
         }
+    }
 };
