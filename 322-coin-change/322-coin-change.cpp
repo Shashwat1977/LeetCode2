ee839cfe3c9d7cc9766174d5dd5a5737438e89c5
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-07-08 15:44:13 +0530

    Time: 148 ms (42.52%), Space: 45.3 MB (21.19%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,31 +1,22 @@
 class Solution {
 public:
-    int coinChange(vector<int>& arr, int x) {
-        int n = arr.size();
-        vector<vector<int>> dp(n,vector<int>(x+1,1e9));
-        for(int i = 0;i<=x;i++){
-            if(i%arr[0] == 0){
-                dp[0][i] = i/arr[0];
-            }
-        }
-        for(int i = 0;i<n;i++){
-            dp[i][0] = 0;
-        }
-        for(int i = 1;i<n;i++){
-            for(int j = 0;j<=x;j++){
-                int no = dp[i-1][j];
-                int pick = 1e9;
-                if(arr[i]<=j){
-                    pick = 1+dp[i][j-arr[i]];
-                }
-                dp[i][j] = min(pick,no);
-            }
-        }
-        if(dp[n-1][x] != 1e9){
-            return dp[n-1][x];
-        }else{
-            return -1;
-        }
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n,vector<int> (amount+1,-1));
+        int ans = helper(n-1,coins,amount,dp);
+        if(ans >= 1e9) return -1;
+        return ans;
     }
-    
+    int helper(int i,vector<int>& coins,int target,vector<vector<int>>& dp){
+        if(target<0) return 1e9;
+        if(target == 0) return 0;
+        if(i == 0){
+            if(target%coins[i] == 0) return target/coins[i];
+            else return 1e9;
+        }
+        if(dp[i][target]!= -1) return dp[i][target];
+        int pick = helper(i,coins,target-coins[i],dp)+1;
+        int non = helper(i-1,coins,target,dp);
+        return dp[i][target] = min(pick,non);
+    }
 };
