78ec8a56768c91fd0d84bcbe92c0e2acca8fd60b
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-07-13 10:05:25 +0530

    Time: 452 ms (28.30%), Space: 12 MB (53.64%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,28 +1,25 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& vec) {
-int sum = accumulate(vec.begin(),vec.end(),0);
-        if(sum%2){
+    bool canPartition(vector<int>& nums) {
+        int sum = accumulate(nums.begin(),nums.end(),0);
+        if(sum%2!= 0){
             return false;
         }
-        int target = sum/2;
-        int n = vec.size();
-        vector<vector<bool>> dp(n,vector<bool>(target+1,false));
+        sum/=2;
+        int n = nums.size();
+        vector<vector<bool>> dp(n,vector<bool> (sum+1,0));
         for(int i = 0;i<n;i++){
-            dp[i][0] = true;
+            dp[i][0] = 1;
         }
-        if(vec[0]<=target) dp[0][vec[0]] = true;
-        for(int i = 1;i<n;i++){
-            for(int j = 0;j<=target;j++){
-                bool npick = dp[i-1][j];
+        if(sum>=nums[0]) dp[0][nums[0]]=1;
+        for(int i= 1;i<n;i++){
+            for(int j = 0;j<=sum;j++){
+                bool non = dp[i-1][j];
                 bool pick = false;
-                if(vec[i]<=j){
-                    pick = dp[i-1][j-vec[i]];
-                }
-                dp[i][j] = pick||npick;
+                if(nums[i]<=j) pick = dp[i-1][j-nums[i]];
+                dp[i][j] = non||pick;
             }
         }
-        return dp[n-1][target];
+        return dp[n-1][sum];
     }
-    
 };
