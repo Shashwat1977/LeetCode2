c7980f96008d488df300097071f9aeaadbcec3ff
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-07-15 14:36:51 +0530

    Time: 42 ms (31.58%), Space: 13 MB (37.20%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.cpp
@@ -1,61 +1,42 @@
 class Solution {
 public:
-    string shortestCommonSupersequence(string s1, string s2) {
-        int n=s1.size(); 
-        int m=s2.size();
-        int t[n+1][m+1]; //table generated while computing LCS length
-        string res; // result	
-        // compute LCS length using tabulation      
-    for(int i=0;i<=n;i++)
-    {
-        for(int j=0;j<=m;j++)
-        {
-            if(i==0||j==0)
-                t[i][j]=0;
-            else if(s1[i-1]==s2[j-1])
-            {
-                t[i][j]=1+t[i-1][j-1];
-            }
-            else
-            {
-                t[i][j]=max(t[i-1][j],t[i][j-1]);
+    string shortestCommonSupersequence(string str1, string str2) {
+        int n = str1.size();int m = str2.size();
+        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(str1[i-1] == str2[j-1]){
+                    dp[i][j] = 1+dp[i-1][j-1];
+                }else{
+                    dp[i][j] = max(dp[i-1][j],dp[i][j-1]);
+                }
             }
         }
-    }     
-       //print lcs
-    int i=n,j=m;
-    while(i>0 && j>0)
-    {
-        if(s1[i-1]==s2[j-1])
-        {
-            res.push_back(s1[i-1]);
+        string temp = "";
+        int i = n;
+        int j = m;
+        while(i>0 && j>0){
+            if(str1[i-1]==str2[j-1]){
+                temp+=str1[i-1];
+                i--;j--;
+            }else{
+                if(dp[i-1][j]>dp[i][j-1]){
+                    temp+=str1[i-1];
+                    i--;
+                }else{
+                    temp+=str2[j-1];
+                    j--;
+                }
+            }
+        }
+        while(i>0){
+            temp+=str1[i-1];
             i--;
+        }while(j>0){
+            temp+=str2[j-1];
             j--;
         }
-        else
-        {
-            if(t[i-1][j]>t[i][j-1])
-            {
-                res.push_back(s1[i-1]);
-                i--;
-            } else{
-                res.push_back(s2[j-1]);
-                j--;
-            }
-        }
-    }
-    while(i>0) // if s1 characters are still left
-    {
-        res.push_back(s1[i-1]);
-        i--;
-    }
-    while(j>0) //if s2 characters are still left
-    {
-        res.push_back(s2[j-1]);
-        j--;
-        
-    }
-        reverse(res.begin(),res.end()); 
-        return res;
+        reverse(temp.begin(),temp.end());
+        return temp;
     }
 };
