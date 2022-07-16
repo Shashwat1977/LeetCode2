b369a91bc1b6206ea3609c64beae018c7beee004
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-07-16 10:54:24 +0530

    Time: 6 ms (20.60%), Space: 8.3 MB (86.31%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -18,7 +18,6 @@ public:
     }
     void postorder(TreeNode* root,vector<int>& ans){
         if(root == NULL) return;
-        
         postorder(root->left,ans);
         postorder(root->right,ans);
         ans.push_back(root->val);
