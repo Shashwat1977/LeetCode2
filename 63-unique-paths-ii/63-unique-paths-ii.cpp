e22f0d070719f99827e8f3c0d3bef5d178c02139
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-07-21 08:47:02 +0530

    Time: 4 ms (64.45%), Space: 7.8 MB (63.64%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& g) {
-        int n = g.size();
+        int n= g.size();
         int m = g[0].size();
-        vector<vector<int>> dp(n,vector<int>(m,0));
+        vector<int> prev(m,0);
         for(int i = 0;i<n;i++){
+            vector<int> temp(m,0);
             for(int j = 0;j<m;j++){
-                if(g[i][j] == 1) dp[i][j] == 0;
+                if(g[i][j] == 1) temp[j] = 0;
                 else{
-                if(i == 0 && j == 0){ dp[i][j] = 1;}
-                else{
-                int l = 0;
-                int u = 0;
-                if(i>0) l = dp[i-1][j];
-                if(j>0) u = dp[i][j-1];
-                dp[i][j] = l+u;
-                }
+                    if(i == 0 && j==0) temp[j] = 1;
+                    else{
+                        temp[j] = ((i>0)?prev[j]:0)+((j>0)?temp[j-1]:0);
+                    }
                 }
             }
+            prev = temp;
         }
-        return dp[n-1][m-1];
+        return prev[m-1];
     }
 };
