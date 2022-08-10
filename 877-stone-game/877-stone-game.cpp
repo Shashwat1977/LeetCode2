3b241e87df1a070f1d67008f42a41c65c9b44d5d
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-08-10 08:37:47 +0530

    Time: 40 ms (22.28%), Space: 16.5 MB (21.60%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
     bool stoneGame(vector<int>& piles) {
-        return true;
+        int n = piles.size();
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        int val = func(0,n-1,piles,dp);
+        if(val>0) return true;
+        else return false;
+    }
+    int func(int i,int j,vector<int>& piles,vector<vector<int>>& dp){
+        if(i == j) return piles[i];
+        if(dp[i][j] != -1) return dp[i][j];
+        int one = piles[i]+func(i+1,j,piles,dp);
+        int two = piles[j]+func(i,j-1,piles,dp);
+        return dp[i][j] = max(one,two);
     }
 };
