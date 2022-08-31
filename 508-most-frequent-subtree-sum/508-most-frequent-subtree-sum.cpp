f80e38357b3e156dab8af3ca47f6f187523001cd
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-08-31 22:51:00 +0530

    Time: 44 ms (14.82%), Space: 25.1 MB (5.74%) - LeetHub

--- 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -12,21 +12,23 @@
 class Solution {
 public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
-        map<long long,int> mp;
-       int val =  helper(root,mp);
+        map<int,int> mp;
+        vector<int> ans;
+        int val = dfs(root,mp);
         map<int,vector<int>> mp2;
         for(auto it:mp){
             mp2[it.second].push_back(it.first);
-            
         }
-        return (*mp2.rbegin()).second;
+        
+        return (--mp2.end())->second;
     }
-    int helper(TreeNode* root,map<long long,int>& mp){
+    int dfs(TreeNode* root,map<int,int>& mp){
         if(root == NULL) return 0;
-        int ans = root->val;
-        ans += helper(root->left,mp);
-        ans += helper(root->right,mp);
-        mp[ans]++;
-        return ans;
+        int sum = root->val;
+        int left = dfs(root->left,mp);
+        int right = dfs(root->right,mp);
+        int total = sum+left+right;
+        mp[total]++;
+        return total;
     }
 };
