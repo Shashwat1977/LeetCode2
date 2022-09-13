27c0c5eeeda238279a0002664cd7cbc16d62e077
Shashwat1977 <99894747+Shashwat1977@users.noreply.github.com>
2022-09-13 17:33:52 +0530

    Time: 7 ms (48.36%), Space: 7.7 MB (23.61%) - LeetHub

--- 486-predict-the-winner/486-predict-the-winner.cpp
+++ 486-predict-the-winner/486-predict-the-winner.cpp
@@ -3,15 +3,16 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         int n = nums.size();
         vector<vector<int>> dp(n,vector<int> (n,-1));
-        int val = func(0,n-1,dp,nums);
-        if(val>=0) return true;
-        return false;
+        int i = 0;
+        int j = n-1;
+        int sum = helper(0,n-1,dp,nums);
+         return 2*sum >= accumulate(nums.begin(), nums.end(), 0);
     }
-    int func(int i,int j,vector<vector<int>>& dp,vector<int>& nums){
-        if(i == j) return nums[i];
+    int helper(int i,int j,vector<vector<int>>& dp,vector<int>& nums){
+        if(i>j) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        int one = nums[i]-func(i+1,j,dp,nums);
-        int two = nums[j] - func(i,j-1,dp,nums);
+        int one = nums[i]+min(helper(i+2,j,dp,nums),helper(i+1,j-1,dp,nums));
+        int two = nums[j] + min(helper(i+1,j-1,dp,nums),helper(i,j-2,dp,nums));
         return dp[i][j] = max(one,two);
     }
 };
